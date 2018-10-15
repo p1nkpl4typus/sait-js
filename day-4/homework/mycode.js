696e3d7271015b822ffa787d7f023fff315c6f7d
Shelagh <34905807+p1nkpl4typus@users.noreply.github.com>
2018-10-14 20:38:59 -0600

    completed homework day 4

--- day-4/homework/mycode.js
+++ day-4/homework/mycode.js
@@ -2,67 +2,46 @@ let quoteBlock = document.getElementById("quote");
 let author = document.getElementsByTagName("cite")[0];
 let nextQuote = document.getElementById("next");
 let prevQuote = document.getElementById("back");
+let tweet = document.getElementById("twitter");
 let currentIndex = 0;
 
 let quotes = [
-    {pos: 0, line: "<p>People are like music. Some speak the truth, and others are just noise.</p>", author: "- Bill Murray (start)"},
-    {pos: 1, line: "<p>Follow your passion. Stay true to yourself. Never follow someone else’s path unless you’re in the woods and you’re lost and you see a path. By all means, you should follow that.</p>", author: "- Ellen DeGeneres (quote 2)"},
-    {pos: 2, line: "<p>Love what you do. Get good at it. Competence is a rare commodity in this day and age.</p>", author: "- Jon Stewart (quote 3)"},
-    {pos: 3, line: "<p>Laughter rises out of tragedy, when you need it the most, and rewards you for your courage.</p>", author: "- Erma Bombeck (quote 4)"},
-    {pos: 4, line: "<p>Tomorrow is often the busiest day of the week.</p>", author: "- Spanish Proverb (end)"}
+    {line: "People are like music. Some speak the truth, and others are just noise.", author: "- Bill Murray"},
+    {line: "Follow your passion. Stay true to yourself. Never follow someone else’s path unless you’re in the woods and you’re lost and you see a path. By all means, you should follow that.", author: "- Ellen DeGeneres"},
+    {line: "Love what you do. Get good at it. Competence is a rare commodity in this day and age.", author: "- Jon Stewart"},
+    {line: "Laughter rises out of tragedy, when you need it the most, and rewards you for your courage.", author: "- Erma Bombeck"},
+    {line: "Tomorrow is often the busiest day of the week.", author: "- Spanish Proverb"}
 ]
 
-function changeQuote() {
-    quoteBlock.innerHTML = quotes[currentIndex].line;
-    author.innerHTML = quotes[currentIndex].author;
-}
-
-function testFunction(startPos) {
+function changeQuote(startPos, index) {
     for (var i = startPos; i < quotes.length; i++){
     } 
-    console.log(currentIndex);
-    changeQuote();
+    quoteBlock.innerHTML = "<p>" + quotes[index].line + "</p>";
+    author.innerHTML = quotes[index].author;
 }
 
 function autoPlay() {
-        setInterval(function(){
-            currentIndex >= quotes.length-1 ? currentIndex = 0 : currentIndex++;
-            testFunction(currentIndex)
-        }, 2000);
-    }
-
-// autoPlay();
-
+    setInterval(function(){
+        currentIndex >= quotes.length-1 ? currentIndex = 0 : currentIndex++;
+        changeQuote(0, currentIndex);
+    }, 30000);
+}
 
 function getNext() {
-    if (currentIndex === 0) {
-        currentIndex = 1;
-    }
-
-    if (currentIndex < 5) {
-        changeQuote();
-        currentIndex++;
-    } 
-
-    if (currentIndex === 5) {
-        currentIndex = 4;
-    }
+    currentIndex >= quotes.length-1 ? currentIndex = 0 : currentIndex++;
+    changeQuote(0, currentIndex);
 }
 
 function getPrev(){
-    if (currentIndex === 4) {
-        currentIndex = 3;
-    }
-
-    if (currentIndex > -1) {
-        changeQuote();
-        currentIndex--;        
-    }
-
-    if (currentIndex === -1) {
-        currentIndex = 0;
-    }
+    currentIndex <=0 ? currentIndex = quotes.length-1 : currentIndex--;
+    changeQuote(0, currentIndex);
 }
 
 nextQuote.addEventListener("click", getNext);
 prevQuote.addEventListener("click", getPrev);
+
+autoPlay();
+
+tweet.addEventListener("click", function() {
+    tweet.setAttribute("href", 'https://twitter.com/intent/tweet?text=' + '"' + quotes[currentIndex].line + '" ' + quotes[currentIndex].author);
+})
